d0508d94c2ad4608f6c4a4d55b0bcaac31ba50b0
Reza Mofidi <64504000+reza-mofidi@users.noreply.github.com>
2020-05-24 01:33:39 +0100

    debugging
    a number of corrections to the code

--- assignment.R
+++ assignment.R
@@ -13,17 +13,17 @@ setwd("~/data/UCI HAR Dataset")
 
 ##assigning dataframes to each file in the dataset
 
-features<- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities<- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code","activity"))
-subject_test<- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "Subject")
-x_test<- read.table ("UCI HAR Dataset/test/x_test.txt", colnames=features$functions)
-y_test<- read.table ("UCI HAR Dataset/test/y_test.txt", colnames="code")
-subject_train<- read.table ("UCI HAR Dataset/train/subject_train.txt", colname="subjects")
-x_train<- read.table ("UCI HAR Dataset/train/x_train.txt", colnames=features$functions)
-y_train<- read.table ("UCI HAR Dataset/train/y_train.txt", colnames="code")
+features<- read.table("~/data/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities<- read.table("~/data/UCI HAR Dataset/activity_labels.txt", col.names = c("code","activity"))
+subject_test<- read.table("~/data/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test<- read.table ("~/data/UCI HAR Dataset/test/x_test.txt", col.names=features$functions)
+y_test<- read.table ("~/data/UCI HAR Dataset/test/y_test.txt", col.names="code")
+subject_train<- read.table ("~/data/UCI HAR Dataset/train/subject_train.txt", col.name="subject")
+x_train<- read.table ("~/data/UCI HAR Dataset/train/x_train.txt", col.names=features$functions)
+y_train<- read.table ("~/data/UCI HAR Dataset/train/y_train.txt", col.names="code")
 
 ## First  goal Merge the training and test datasets
-X_complete<- rbind(x_train,X_test)
+x_complete<- rbind(x_train,x_test)
 y_complete<- rbind(y_train, y_test)
 subjects <- rbind(subject_train, subject_test)
 combined_data<- cbind(subjects, x_complete, y_complete)
